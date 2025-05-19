7941cd9eb305ffa6a1b3533f9fef2488c2c46b53
Prakhar Verma <prakharverma2019@gmail.com>
2025-05-20 02:55:43 +0530

    Refactor the error message

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -49,11 +50,7 @@ const NewIssue = () => {
             <TextField.Root placeholder="Title" {...field} />
           )}
         />
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+       <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -61,11 +58,11 @@ const NewIssue = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        
+          <ErrorMessage >
+            {errors.description?.message}
+          </ErrorMessage>
+      
 
         <Button>Submit New Issue</Button>
       </form>
