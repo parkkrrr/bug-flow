276750904c9df5da714c50943b5b4fa42978428c
Prakhar Verma <prakharverma2019@gmail.com>
2025-05-26 15:44:46 +0530

    Refactoring NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenu,
   Flex,
   Text,
-  Button
+  Button,
 } from "@radix-ui/themes";
 import classNames from "classnames";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -16,14 +16,6 @@ import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
-
   return (
     <nav className=" px-5 mb-5 py-3 border-b ">
       <Container>
@@ -32,54 +24,73 @@ const NavBar = () => {
             <Link href="/">
               <FaBug />
             </Link>
-            <ul className="flex space-x-4">
-              {links.map((link) => {
-                const isActive = currentPath === link.href;
-                return (
-                  <li key={link.href}>
-                    <Link
-                      className={classNames(
-                        { "text-zinc-900": isActive },
-                        { "text-zinc-500": !isActive },
-                        { "hover:text-zinc-800 transition-colors": true }
-                      )}
-                      // className={`$(currentPath===link.href?"text-zinc-900":"text-zinc-500) hover:text-zinc-800 transition-colors`}
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user?.image ?? undefined}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item onClick={() => signOut()}>Logout</DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Button onClick={()=>signIn()}>Login</Button>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+  return (
+    <ul className="flex space-x-4">
+      {links.map((link) => {
+        const isActive = currentPath === link.href;
+        return (
+          <li key={link.href}>
+            <Link
+              className={classNames(
+                { "nav-link": true },
+                { "!text-zinc-900": isActive }
+              )}
+              // className={`$(currentPath===link.href?"text-zinc-900":"text-zinc-500) hover:text-zinc-800 transition-colors`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Button onClick={() => signIn()}>Login</Button>;
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image ?? undefined}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item onClick={() => signOut()}>
+            Logout
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  async header() {
+    return [
+      { source: "/:path*" },
+      {
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
